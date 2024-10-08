d28d4223cc844046fe3ee6e33193429ccb4968f9
joaocarlos017 <joaocarlosdev17@gmail.com>
2024-10-08 17:12:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('whatsapp').addEventListener('click', function() {
+    window.open('https://wa.me/55SEUNUMERO', '_blank');
+});
+
+document.getElementById('email').addEventListener('click', function() {
+    window.location.href = 'mailto:seuemail@exemplo.com';
+});
